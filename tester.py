8991eccc198227df79438db2da37ca976ef06027
Julian Gu <j.gush99@gmail.com>
2020-02-17 16:29:33 -0500

    tested sqrt

--- tester.py
+++ tester.py
@@ -6,7 +6,9 @@ from optimizer import deadcode_elim_marksweep
 
 
 def test_quicksort():
-	instructions = parse_instructions("public_test_cases/quicksort/quicksort.ir")
+	# instructions = parse_instructions("public_test_cases/quicksort/quicksort.ir")
+	instructions = parse_instructions("public_test_cases/sqrt/sqrt.ir")
+
 	functions = get_functions(instructions)
 	cfgs = [CFGraph.build(func) for func in functions]
 	# for i in range(len(cfgs[0].instructions)):
@@ -15,7 +17,7 @@ def test_quicksort():
 	for cfg in cfgs:
 		deadcode_elim_marksweep(cfg)
 		optimized_instructions += cfg.instructions
-	generate_ir(optimized_instructions, "public_test_cases/examples/quicksort.ir")
+	generate_ir(optimized_instructions, "public_test_cases/examples/sqrt.ir")
 
 def main():
 	test_quicksort()

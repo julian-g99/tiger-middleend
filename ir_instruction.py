08c668c5435c9e87eecf530ba49a0b8a3a76bedb
Julian Gu <j.gush99@gmail.com>
2020-03-22 19:37:14 -0400

    tabs to spaces

--- ir_instruction.py
+++ ir_instruction.py
@@ -1,170 +1,170 @@
 class IRInstruction:
-	def __init__(self, line, instruction_type, argument_list):
-		self.line = line
-		self.instruction_type = instruction_type
-		self.argument_list = argument_list
+    def __init__(self, line, instruction_type, argument_list):
+        self.line = line
+        self.instruction_type = instruction_type
+        self.argument_list = argument_list
 
-		def_arr = ['val_assign',
-			 'sub',
-			 'add',
-			 'mult',
-			 'div',
-			 'and',
-			 'or',
-			 'callr',
-			 'array_load',
-			 'array_assign']
-		use_arr = ['val_assign',
-			 'sub',
-			 'add',
-			 'mult',
-			 'div',
-			 'and',
-			 'or',
-			 'breq',
-			 'brneq',
-			 'brlt',
-			 'brgt',
-			 'brleq',
-			 'brgeq',
-			 'return',
-			 'call',
-			 'callr',
-			 'array_store',
-			 'array_load',
-			 'array_assign']
-		critical_arr = ['breq',
-					 'brneq',
-					 'brlt',
-					 'brgt',
-					 'brgeq',
-					 'brleq',
-					 'goto',
-					 'function_start',
-					 'function_def',
-					 'function_end',
-					 'function_int_decl',
-					 'function_float_decl',
-					 'label',
-					 'call',
-					 'callr',
-					 'array_load',
-					 'array_store',
-					 'return',
-					 'array_assign']
-		branch_arr = ['breq',
-				   'brneq',
-				   'brlt',
-				   'brgt',
-				   'brgeq',
-				   'brleq',
-				   'goto']
+        def_arr = ['val_assign',
+             'sub',
+             'add',
+             'mult',
+             'div',
+             'and',
+             'or',
+             'callr',
+             'array_load',
+             'array_assign']
+        use_arr = ['val_assign',
+             'sub',
+             'add',
+             'mult',
+             'div',
+             'and',
+             'or',
+             'breq',
+             'brneq',
+             'brlt',
+             'brgt',
+             'brleq',
+             'brgeq',
+             'return',
+             'call',
+             'callr',
+             'array_store',
+             'array_load',
+             'array_assign']
+        critical_arr = ['breq',
+                     'brneq',
+                     'brlt',
+                     'brgt',
+                     'brgeq',
+                     'brleq',
+                     'goto',
+                     'function_start',
+                     'function_def',
+                     'function_end',
+                     'function_int_decl',
+                     'function_float_decl',
+                     'label',
+                     'call',
+                     'callr',
+                     'array_load',
+                     'array_store',
+                     'return',
+                     'array_assign']
+        branch_arr = ['breq',
+                   'brneq',
+                   'brlt',
+                   'brgt',
+                   'brgeq',
+                   'brleq',
+                   'goto']
 
-		self.is_def = self.instruction_type in def_arr
-		self.is_use = self.instruction_type in use_arr
-		self.is_critical = self.instruction_type in critical_arr
-		self.is_branch = self.instruction_type in branch_arr
-		self.is_goto = self.instruction_type == "goto"
-		self.is_label = self.instruction_type == "label"
+        self.is_def = self.instruction_type in def_arr
+        self.is_use = self.instruction_type in use_arr
+        self.is_critical = self.instruction_type in critical_arr
+        self.is_branch = self.instruction_type in branch_arr
+        self.is_goto = self.instruction_type == "goto"
+        self.is_label = self.instruction_type == "label"
 
-		# self.succs = []
-		# self.preds = []
+        # self.succs = []
+        # self.preds = []
 
-	def does_kill(self, other_def):
-		return self.argument_list[0] == other_def.argument_list[0]
+    def does_kill(self, other_def):
+        return self.argument_list[0] == other_def.argument_list[0]
 
-	def get_uses(self):
-		binary_instructions = ['add', 'sub', 'mult', 'div', 'and', 'or']
-		branches = ['breq', 'brneq', 'brlt', 'brgt', 'brgeq', 'brleq']
-		if self.instruction_type == "val_assign":
-			return self.argument_list[1:2]
-		elif self.instruction_type in binary_instructions:
-			return self.argument_list[1:3]
-		elif self.instruction_type in branches:
-			return self.argument_list[1:3]
-		elif self.instruction_type == "callr":
-			return self.argument_list[2:]
-		elif self.instruction_type == "call":
-			return self.argument_list[1:]
-		elif self.instruction_type == "array_store":
-			return self.argument_list[:] #NOTE: the last argument should always be a constant
-		elif self.instruction_type == "return":
-			return self.argument_list[0:1]
-		elif self.instruction_type == "array_load":
-			return self.argument_list[1:]
-		elif self.instruction_type == "array_assign":
-			return self.argument_list[1:]
-		else:
-			return None
-	
-	#TODO: check if this actually works in place
-	def set_use(self, idx, arg):
-		binary_instructions = ['add', 'sub', 'mult', 'div', 'and', 'or']
-		branches = ['breq', 'brneq', 'brlt', 'brgt', 'brgeq', 'brleq']
-		# print("set use called on index: {}, arg: {}".format(idx, arg))
-		if self.instruction_type == "val_assign":
-			if idx != 0:
-				print("set use out of bounds. instruction:{}, idx: {}, arg:{}"\
-				  .format(self.__str__(), idx, arg))
-			else:
-				self.argument_list[1] = arg
-		elif self.instruction_type in binary_instructions:
-			if idx >= 2:
-				print("set use out of bounds. instruction:{}, idx: {}, arg:{}"\
-				  .format(self.__str__(), idx, arg))
-			else:
-				self.argument_list[idx+1] = arg
-		elif self.instruction_type in branches:
-			if idx >= 2:
-				print("set use out of bounds. instruction:{}, idx: {}, arg:{}"\
-				  .format(self.__str__(), idx, arg))
-			else:
-				self.argument_list[idx+1] = arg
-		elif self.instruction_type == "callr":
-			self.argument_list[idx+2] = arg
-		elif self.instruction_type == "call":
-			self.argument_list[idx+1] = arg
-		elif self.instruction_type == "array_store":
-			if idx >= 3:
-				print("set use out of bounds. instruction:{}, idx: {}, arg:{}"\
-				  .format(self.__str__(), idx, arg))
-			else:
-				self.argument_list[idx] = arg
-		elif self.instruction_type == "return":
-			if idx >= 1:
-				print("set use out of bounds. instruction:{}, idx: {}, arg:{}"\
-				  .format(self.__str__(), idx, arg))
-			else:
-				self.argument_list[idx] = arg
-		elif self.instruction_type == "array_load":
-			if idx >= 2:
-				print("set use out of bounds. instruction:{}, idx: {}, arg:{}"\
-				  .format(self.__str__(), idx, arg))
-			else:
-				self.argument_list[idx + 1] = arg
-		elif self.instruction_type == "array_assign":
-			if idx >= 3:
-				print("set use out of bounds. instruction:{}, idx: {}, arg:{}"\
-				  .format(self.__str__(), idx, arg))
-			else:
-				self.argument_list[idx] = arg
+    def get_uses(self):
+        binary_instructions = ['add', 'sub', 'mult', 'div', 'and', 'or']
+        branches = ['breq', 'brneq', 'brlt', 'brgt', 'brgeq', 'brleq']
+        if self.instruction_type == "val_assign":
+            return self.argument_list[1:2]
+        elif self.instruction_type in binary_instructions:
+            return self.argument_list[1:3]
+        elif self.instruction_type in branches:
+            return self.argument_list[1:3]
+        elif self.instruction_type == "callr":
+            return self.argument_list[2:]
+        elif self.instruction_type == "call":
+            return self.argument_list[1:]
+        elif self.instruction_type == "array_store":
+            return self.argument_list[:] #NOTE: the last argument should always be a constant
+        elif self.instruction_type == "return":
+            return self.argument_list[0:1]
+        elif self.instruction_type == "array_load":
+            return self.argument_list[1:]
+        elif self.instruction_type == "array_assign":
+            return self.argument_list[1:]
+        else:
+            return None
+    
+    #TODO: check if this actually works in place
+    def set_use(self, idx, arg):
+        binary_instructions = ['add', 'sub', 'mult', 'div', 'and', 'or']
+        branches = ['breq', 'brneq', 'brlt', 'brgt', 'brgeq', 'brleq']
+        # print("set use called on index: {}, arg: {}".format(idx, arg))
+        if self.instruction_type == "val_assign":
+            if idx != 0:
+                print("set use out of bounds. instruction:{}, idx: {}, arg:{}"\
+                  .format(self.__str__(), idx, arg))
+            else:
+                self.argument_list[1] = arg
+        elif self.instruction_type in binary_instructions:
+            if idx >= 2:
+                print("set use out of bounds. instruction:{}, idx: {}, arg:{}"\
+                  .format(self.__str__(), idx, arg))
+            else:
+                self.argument_list[idx+1] = arg
+        elif self.instruction_type in branches:
+            if idx >= 2:
+                print("set use out of bounds. instruction:{}, idx: {}, arg:{}"\
+                  .format(self.__str__(), idx, arg))
+            else:
+                self.argument_list[idx+1] = arg
+        elif self.instruction_type == "callr":
+            self.argument_list[idx+2] = arg
+        elif self.instruction_type == "call":
+            self.argument_list[idx+1] = arg
+        elif self.instruction_type == "array_store":
+            if idx >= 3:
+                print("set use out of bounds. instruction:{}, idx: {}, arg:{}"\
+                  .format(self.__str__(), idx, arg))
+            else:
+                self.argument_list[idx] = arg
+        elif self.instruction_type == "return":
+            if idx >= 1:
+                print("set use out of bounds. instruction:{}, idx: {}, arg:{}"\
+                  .format(self.__str__(), idx, arg))
+            else:
+                self.argument_list[idx] = arg
+        elif self.instruction_type == "array_load":
+            if idx >= 2:
+                print("set use out of bounds. instruction:{}, idx: {}, arg:{}"\
+                  .format(self.__str__(), idx, arg))
+            else:
+                self.argument_list[idx + 1] = arg
+        elif self.instruction_type == "array_assign":
+            if idx >= 3:
+                print("set use out of bounds. instruction:{}, idx: {}, arg:{}"\
+                  .format(self.__str__(), idx, arg))
+            else:
+                self.argument_list[idx] = arg
 
 
-	def get_write_target(self):
-		has_target = ['val_assign', 'array_assign', 'sub', 'add', 'mult', 'div', 'and', 'or', 'callr', 'array_load']
-		if self.instruction_type in has_target:
-			return self.argument_list[0].strip()
-		else:
-			return None
+    def get_write_target(self):
+        has_target = ['val_assign', 'array_assign', 'sub', 'add', 'mult', 'div', 'and', 'or', 'callr', 'array_load']
+        if self.instruction_type in has_target:
+            return self.argument_list[0].strip()
+        else:
+            return None
 
-	def get_branch_target(self):
-		if self.is_branch:
-			return self.argument_list[0]
-	
-	def get_label(self):
-		if self.is_label:
-			return self.argument_list[0]
+    def get_branch_target(self):
+        if self.is_branch:
+            return self.argument_list[0]
+    
+    def get_label(self):
+        if self.is_label:
+            return self.argument_list[0]
 
-	
-	def __str__(self):
-		return "line number: {}, type: {}, argument_list: {}".format(self.line, self.instruction_type, self.argument_list)
+    
+    def __str__(self):
+        return "line number: {}, type: {}, argument_list: {}".format(self.line, self.instruction_type, self.argument_list)
--- parser.py
+++ parser.py
@@ -3,77 +3,77 @@ from ir_instruction import IRInstruction
 
 
 def parse_instructions(fp):
-	function_regex = r'^.+ .+(.*):$'
-	instructions = []
-	with open(fp, 'r') as file:
-		line_num = 0
-		for line in file:
-			if "#start_function" in line:
-				instructions.append(IRInstruction(line_num, "function_start", []))
-			elif "#end_function" in line:
-				instructions.append(IRInstruction(line_num, "function_end", []))
-			elif "int-list:" in line:
-				instructions.append(IRInstruction(line_num, "function_int_decl", get_variables(line)))
-			elif "float-list:" in line:
-				instructions.append(IRInstruction(line_num, "function_float_decl", get_variables(line)))
-			elif re.match(function_regex, line) != None:
-				instructions.append(IRInstruction(line_num, "function_def", [line]))
-			elif ":" in line:
-				instructions.append(IRInstruction(line_num, "label", [line[len(line) - len(line.strip()) - 1 : line.find(":")]]))
-			elif "assign" in line:
-				arg_list = get_arguments(line)
-				if len(arg_list) == 2:
-					instructions.append(IRInstruction(line_num, "val_assign", arg_list))
-				elif len(arg_list) == 3:
-					instructions.append(IRInstruction(line_num, "array_assign", arg_list))
-			elif line.strip() == "" :
-				continue
-			else:
-				opcode = line[len(line) - len(line.strip()) - 1 : line.find(',')]
-				arg_list = get_arguments(line)
-				instructions.append(IRInstruction(line_num, opcode, arg_list))
-			line_num += 1
-	return instructions
+    function_regex = r'^.+ .+(.*):$'
+    instructions = []
+    with open(fp, 'r') as file:
+        line_num = 0
+        for line in file:
+            if "#start_function" in line:
+                instructions.append(IRInstruction(line_num, "function_start", []))
+            elif "#end_function" in line:
+                instructions.append(IRInstruction(line_num, "function_end", []))
+            elif "int-list:" in line:
+                instructions.append(IRInstruction(line_num, "function_int_decl", get_variables(line)))
+            elif "float-list:" in line:
+                instructions.append(IRInstruction(line_num, "function_float_decl", get_variables(line)))
+            elif re.match(function_regex, line) != None:
+                instructions.append(IRInstruction(line_num, "function_def", [line]))
+            elif ":" in line:
+                instructions.append(IRInstruction(line_num, "label", [line[len(line) - len(line.strip()) - 1 : line.find(":")]]))
+            elif "assign" in line:
+                arg_list = get_arguments(line)
+                if len(arg_list) == 2:
+                    instructions.append(IRInstruction(line_num, "val_assign", arg_list))
+                elif len(arg_list) == 3:
+                    instructions.append(IRInstruction(line_num, "array_assign", arg_list))
+            elif line.strip() == "" :
+                continue
+            else:
+                opcode = line[len(line) - len(line.strip()) - 1 : line.find(',')]
+                arg_list = get_arguments(line)
+                instructions.append(IRInstruction(line_num, opcode, arg_list))
+            line_num += 1
+    return instructions
 
 
 def get_functions(instructions):
-	functions = []
-	line = 0
-	while line < len(instructions):
-		if instructions[line].instruction_type == 'function_start':
-			functions.append([instructions[line]])
-			line += 1
-			while line < len(instructions) and instructions[line].instruction_type != 'function_end':
-				functions[-1].append(instructions[line])
-				line += 1
-			if (line < len(instructions) and instructions[line].instruction_type == 'function_end'):
-				functions[-1].append(instructions[line])
-			line += 1
-	return functions
+    functions = []
+    line = 0
+    while line < len(instructions):
+        if instructions[line].instruction_type == 'function_start':
+            functions.append([instructions[line]])
+            line += 1
+            while line < len(instructions) and instructions[line].instruction_type != 'function_end':
+                functions[-1].append(instructions[line])
+                line += 1
+            if (line < len(instructions) and instructions[line].instruction_type == 'function_end'):
+                functions[-1].append(instructions[line])
+            line += 1
+    return functions
 
 
 def flatten_functions(functions):
-	instructions = []
-	for func in functions:
-		for instr in func:
-			instructions.append(instr)
-	return instructions
+    instructions = []
+    for func in functions:
+        for instr in func:
+            instructions.append(instr)
+    return instructions
 
 
 def get_arguments(line):
-	return [arg.strip() for arg in line.split(", ")[1:]]
+    return [arg.strip() for arg in line.split(", ")[1:]]
 
 
 def get_variables(line):
-	colon_index = line.find(": ")
-	if colon_index != -1:
-		var_sub = line[colon_index + 2:]
-		return [var.strip() for var in var_sub.split(", ")]
-	else:
-		return []
+    colon_index = line.find(": ")
+    if colon_index != -1:
+        var_sub = line[colon_index + 2:]
+        return [var.strip() for var in var_sub.split(", ")]
+    else:
+        return []
 
 
 if __name__ == "__main__":
-	instructions = parse_instructions("public_test_cases/quicksort/quicksort.ir")
-	for i in instructions:
-		print(i)
+    instructions = parse_instructions("public_test_cases/quicksort/quicksort.ir")
+    for i in instructions:
+        print(i)
